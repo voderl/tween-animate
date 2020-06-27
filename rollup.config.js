efaed8af9f2eac4926f917cd96eedd9ae1c11a34
voderl <voderl@qq.com>
2020-06-27 16:03:15 +0800

    change webpack to rollup

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import pkg from './package.json';
+import typescript from '@rollup/plugin-typescript';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
+//terser API : https://github.com/terser/terser#compress-options
+import babel from '@rollup/plugin-babel';
+
+export default {
+  input: 'src/index.js',
+  output: {
+    name: 'tween',
+    file: pkg.main,
+    format: 'umd',
+    sourcemap: true,
+  },
+  plugins: [resolve(), commonjs(), typescript()].concat(
+    process.env.BUILD === 'production'
+      ? [babel({ babelHelpers: 'bundled' }), terser()]
+      : [],
+  ),
+};
