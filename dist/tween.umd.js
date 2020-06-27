6b04c2f126e39ab34ec084821aac01106d05a933
voderl <voderl@qq.com>
2020-06-27 19:38:31 +0800

    update

--- dist/tween.umd.js
+++ dist/tween.umd.js
@@ -1021,241 +1021,237 @@
 	/**
 	 *  Vue props validate
 	 */
+	var Types;
+	(function (Types) {
+	    Types[Types["String"] = 0] = "String";
+	    Types[Types["Number"] = 1] = "Number";
+	    Types[Types["Boolean"] = 2] = "Boolean";
+	    Types[Types["Function"] = 3] = "Function";
+	    Types[Types["Symbol"] = 4] = "Symbol";
+	})(Types || (Types = {}));
 	function warn(data) {
-	  throw new Error(data);
+	    throw new Error(data);
 	}
-	const hasOwnProperty = Object.prototype.hasOwnProperty;
+	var hasOwnProperty = Object.prototype.hasOwnProperty;
 	function hasOwn(obj, key) {
-	  return hasOwnProperty.call(obj, key);
+	    return hasOwnProperty.call(obj, key);
 	}
 	function isObject(obj) {
-	  return obj !== null && typeof obj === 'object';
+	    return obj !== null && typeof obj === 'object';
 	}
-	const _toString = Object.prototype.toString;
+	var _toString = Object.prototype.toString;
 	function toRawType(value) {
-	  return _toString.call(value).slice(8, -1);
+	    return _toString.call(value).slice(8, -1);
 	}
 	function cached(fn) {
-	  const cache = Object.create(null);
-	  return function cachedFn(str) {
-	    const hit = cache[str];
-	    return hit || (cache[str] = fn(str));
-	  };
+	    var cache = Object.create(null);
+	    return function cachedFn(str) {
+	        var hit = cache[str];
+	        return hit || (cache[str] = fn(str));
+	    };
 	}
 	/**
 	 * Hyphenate a camelCase string.
 	 */
-	const hyphenateRE = /\B([A-Z])/g;
-	const hyphenate = cached((str) => {
-	  return str.replace(hyphenateRE, '-$1').toLowerCase();
+	var hyphenateRE = /\B([A-Z])/g;
+	var hyphenate = cached(function (str) {
+	    return str.replace(hyphenateRE, '-$1').toLowerCase();
 	});
-
 	/**
 	 * Capitalize a string.
 	 */
-	const capitalize = cached((str) => {
-	  return str.charAt(0).toUpperCase() + str.slice(1);
+	var capitalize = cached(function (str) {
+	    return str.charAt(0).toUpperCase() + str.slice(1);
 	});
 	function isPlainObject(obj) {
-	  return _toString.call(obj) === '[object Object]';
+	    return _toString.call(obj) === '[object Object]';
 	}
-
 	function validate(propOptions, propsData) {
-	  Object.keys(propOptions).forEach((key) => {
-	    propsData[key] = validateProp(key, propOptions, propsData);
-	  });
-	  return propsData;
+	    Object.keys(propOptions).forEach(function (key) {
+	        propsData[key] = validateProp(key, propOptions, propsData);
+	    });
+	    return propsData;
 	}
-
 	function validateProp(key, propOptions, propsData) {
-	  const prop = propOptions[key];
-	  const absent = !hasOwn(propsData, key);
-	  let value = propsData[key];
-	  // boolean casting
-	  const booleanIndex = getTypeIndex(Boolean, prop.type);
-	  if (booleanIndex > -1) {
-	    if (absent && !hasOwn(prop, 'default')) {
-	      value = false;
-	    } else if (value === '' || value === hyphenate(key)) {
-	      // only cast empty string / same name to boolean if
-	      // boolean has higher priority
-	      const stringIndex = getTypeIndex(String, prop.type);
-	      if (stringIndex < 0 || booleanIndex < stringIndex) {
-	        value = true;
-	      }
+	    var prop = propOptions[key];
+	    var absent = !hasOwn(propsData, key);
+	    var value = propsData[key];
+	    // boolean casting
+	    var booleanIndex = getTypeIndex(Boolean, prop.type);
+	    if (booleanIndex > -1) {
+	        if (absent && !hasOwn(prop, 'default')) {
+	            value = false;
+	        }
+	        else if (value === '' || value === hyphenate(key)) {
+	            // only cast empty string / same name to boolean if
+	            // boolean has higher priority
+	            var stringIndex = getTypeIndex(String, prop.type);
+	            if (stringIndex < 0 || booleanIndex < stringIndex) {
+	                value = true;
+	            }
+	        }
 	    }
-	  }
-	  // check default value
-	  if (value === undefined) {
-	    value = getPropDefaultValue(prop, key);
-	  }
-	  // if (
-	  //   process.env.NODE_ENV !== 'production'
-	  // ) {
-	  assertProp(prop, key, value, absent);
-	  // }
-	  return value;
+	    // check default value
+	    if (value === undefined) {
+	        value = getPropDefaultValue(prop, key);
+	    }
+	    // if (
+	    //   process.env.NODE_ENV !== 'production'
+	    // ) {
+	    assertProp(prop, key, value, absent);
+	    // }
+	    return value;
 	}
-
 	/**
 	 * Get the default value of a prop.
 	 */
 	function getPropDefaultValue(prop, key) {
-	  // no default, return undefined
-	  if (!hasOwn(prop, 'default')) {
-	    return undefined;
-	  }
-	  const def = prop.default;
-	  // warn against non-factory defaults for Object & Array
-	  if (isObject(def)) {
-	    warn(
-	      'Invalid default value for prop "' +
-	        key +
-	        '": ' +
-	        'Props with type Object/Array must use a factory function ' +
-	        'to return the default value.',
-	    );
-	  }
-	  // the raw prop value was also undefined from previous render,
-	  // return previous default value to avoid unnecessary watcher trigger
-	  // call factory function for non-Function types
-	  // a value is Function if its prototype is function even across different execution context
-	  return typeof def === 'function' && getType(prop.type) !== 'Function'
-	    ? def()
-	    : def;
+	    // no default, return undefined
+	    if (!hasOwn(prop, 'default')) {
+	        return undefined;
+	    }
+	    var def = prop.default;
+	    // warn against non-factory defaults for Object & Array
+	    if (isObject(def)) {
+	        warn('Invalid default value for prop "' +
+	            key +
+	            '": ' +
+	            'Props with type Object/Array must use a factory function ' +
+	            'to return the default value.');
+	    }
+	    // the raw prop value was also undefined from previous render,
+	    // return previous default value to avoid unnecessary watcher trigger
+	    // call factory function for non-Function types
+	    // a value is Function if its prototype is function even across different execution context
+	    return typeof def === 'function' && getType(prop.type) !== 'Function'
+	        ? def()
+	        : def;
 	}
-
 	/**
 	 * Assert whether a prop is valid.
 	 */
 	function assertProp(prop, name, value, absent) {
-	  if (prop.required && absent) {
-	    warn('Missing required prop: "' + name + '"');
-	    return;
-	  }
-	  if (value == null && !prop.required) {
-	    return;
-	  }
-	  let type = prop.type;
-	  let valid = !type || type === true;
-	  const expectedTypes = [];
-	  if (type) {
-	    if (!Array.isArray(type)) {
-	      type = [type];
+	    if (prop.required && absent) {
+	        warn('Missing required prop: "' + name + '"');
+	        return;
 	    }
-	    for (let i = 0; i < type.length && !valid; i++) {
-	      const assertedType = assertType(value, type[i]);
-	      expectedTypes.push(assertedType.expectedType || '');
-	      valid = assertedType.valid;
+	    if (value == null && !prop.required) {
+	        return;
 	    }
-	  }
-
-	  if (!valid) {
-	    warn(getInvalidTypeMessage(name, value, expectedTypes));
-	    return;
-	  }
-	  const validator = prop.validator;
-	  if (validator) {
-	    if (!validator(value)) {
-	      warn(
-	        'Invalid prop: custom validator check failed for prop "' + name + '".',
-	      );
+	    var type = prop.type;
+	    var valid = !type || type === true;
+	    var expectedTypes = [];
+	    if (type) {
+	        if (!Array.isArray(type)) {
+	            type = [type];
+	        }
+	        for (var i = 0; i < type.length && !valid; i++) {
+	            var assertedType = assertType(value, type[i]);
+	            expectedTypes.push(assertedType.expectedType || '');
+	            valid = assertedType.valid;
+	        }
 	    }
-	  }
+	    if (!valid) {
+	        warn(getInvalidTypeMessage(name, value, expectedTypes));
+	        return;
+	    }
+	    var validator = prop.validator;
+	    if (validator) {
+	        if (!validator(value)) {
+	            warn('Invalid prop: custom validator check failed for prop "' + name + '".');
+	        }
+	    }
 	}
-
-	const simpleCheckRE = /^(String|Number|Boolean|Function|Symbol)$/;
-
+	var simpleCheckRE = /^(String|Number|Boolean|Function|Symbol)$/;
 	function assertType(value, type) {
-	  let valid;
-	  const expectedType = getType(type);
-	  if (simpleCheckRE.test(expectedType)) {
-	    const t = typeof value;
-	    valid = t === expectedType.toLowerCase();
-	    // for primitive wrapper objects
-	    if (!valid && t === 'object') {
-	      valid = value instanceof type;
+	    var valid;
+	    var expectedType = getType(type);
+	    if (simpleCheckRE.test(expectedType)) {
+	        var t = typeof value;
+	        valid = t === expectedType.toLowerCase();
+	        // for primitive wrapper objects
+	        if (!valid && t === 'object') {
+	            valid = value instanceof type;
+	        }
 	    }
-	  } else if (expectedType === 'Object') {
-	    valid = isPlainObject(value);
-	  } else if (expectedType === 'Array') {
-	    valid = Array.isArray(value);
-	  } else {
-	    valid = value instanceof type;
-	  }
-	  return {
-	    valid,
-	    expectedType,
-	  };
+	    else if (expectedType === 'Object') {
+	        valid = isPlainObject(value);
+	    }
+	    else if (expectedType === 'Array') {
+	        valid = Array.isArray(value);
+	    }
+	    else {
+	        valid = value instanceof type;
+	    }
+	    return {
+	        valid: valid,
+	        expectedType: expectedType,
+	    };
 	}
-
 	/**
 	 * Use function string name to check built-in types,
 	 * because a simple equality check will fail when running
 	 * across different vms / iframes.
 	 */
 	function getType(fn) {
-	  const match = fn && fn.toString().match(/^\s*function (\w+)/);
-	  return match ? match[1] : '';
+	    var match = fn && fn.toString().match(/^\s*function (\w+)/);
+	    return match ? match[1] : '';
 	}
-
 	function isSameType(a, b) {
-	  return getType(a) === getType(b);
+	    return getType(a) === getType(b);
 	}
-
 	function getTypeIndex(type, expectedTypes) {
-	  if (!Array.isArray(expectedTypes)) {
-	    return isSameType(expectedTypes, type) ? 0 : -1;
-	  }
-	  for (let i = 0, len = expectedTypes.length; i < len; i++) {
-	    if (isSameType(expectedTypes[i], type)) {
-	      return i;
+	    if (!Array.isArray(expectedTypes)) {
+	        return isSameType(expectedTypes, type) ? 0 : -1;
 	    }
-	  }
-	  return -1;
+	    for (var i = 0, len = expectedTypes.length; i < len; i++) {
+	        if (isSameType(expectedTypes[i], type)) {
+	            return i;
+	        }
+	    }
+	    return -1;
 	}
-
 	function getInvalidTypeMessage(name, value, expectedTypes) {
-	  let message =
-	    `Invalid prop: type check failed for prop "${name}".` +
-	    ` Expected ${expectedTypes.map(capitalize).join(', ')}`;
-	  const expectedType = expectedTypes[0];
-	  const receivedType = toRawType(value);
-	  const expectedValue = styleValue(value, expectedType);
-	  const receivedValue = styleValue(value, receivedType);
-	  // check if we need to specify expected value
-	  if (
-	    expectedTypes.length === 1 &&
-	    isExplicable(expectedType) &&
-	    !isBoolean(expectedType, receivedType)
-	  ) {
-	    message += ` with value ${expectedValue}`;
-	  }
-	  message += `, got ${receivedType} `;
-	  // check if we need to specify received value
-	  if (isExplicable(receivedType)) {
-	    message += `with value ${receivedValue}.`;
-	  }
-	  return message;
+	    var message = "Invalid prop: type check failed for prop \"" + name + "\"." +
+	        (" Expected " + expectedTypes.map(capitalize).join(', '));
+	    var expectedType = expectedTypes[0];
+	    var receivedType = toRawType(value);
+	    var expectedValue = styleValue(value, expectedType);
+	    var receivedValue = styleValue(value, receivedType);
+	    // check if we need to specify expected value
+	    if (expectedTypes.length === 1 &&
+	        isExplicable(expectedType) &&
+	        !isBoolean(expectedType, receivedType)) {
+	        message += " with value " + expectedValue;
+	    }
+	    message += ", got " + receivedType + " ";
+	    // check if we need to specify received value
+	    if (isExplicable(receivedType)) {
+	        message += "with value " + receivedValue + ".";
+	    }
+	    return message;
 	}
-
 	function styleValue(value, type) {
-	  if (type === 'String') {
-	    return `"${value}"`;
-	  } else if (type === 'Number') {
-	    return `${Number(value)}`;
-	  } else {
-	    return `${value}`;
-	  }
+	    if (type === 'String') {
+	        return "\"" + value + "\"";
+	    }
+	    else if (type === 'Number') {
+	        return "" + Number(value);
+	    }
+	    else {
+	        return "" + value;
+	    }
 	}
-
 	function isExplicable(value) {
-	  const explicitTypes = ['string', 'number', 'boolean'];
-	  return explicitTypes.some((elem) => value.toLowerCase() === elem);
+	    var explicitTypes = ['string', 'number', 'boolean'];
+	    return explicitTypes.some(function (elem) { return value.toLowerCase() === elem; });
 	}
-
-	function isBoolean(...args) {
-	  return args.some((elem) => elem.toLowerCase() === 'boolean');
+	function isBoolean() {
+	    var args = [];
+	    for (var _i = 0; _i < arguments.length; _i++) {
+	        args[_i] = arguments[_i];
+	    }
+	    return args.some(function (elem) { return elem.toLowerCase() === 'boolean'; });
 	}
 
 	// tween.update(elpased); 不指定则是内部list
